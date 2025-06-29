c3f0bd48e11817b7b06fc037e2071f03d5a2a59c
AJ <hanifquadre3@gmail.com>
2025-06-29 20:31:20 -0300

    While loop added

--- main.lua
+++ main.lua
@@ -1,15 +1,3 @@
--- print type of text as string
-print(type(10))   --> string
-t = 10
-
--- print type of number
-print(type(5.8*t))               --> number
--- print type of boolean
-print(type(67))                --> boolean
--- print type of function
-print(type(45))               --> number
--- print type of nil
-print(type(45))                 --> nil
--- print type of number
-print(type(10))                 --> number
-T = 10
+for i = 1, 10000000, 100 do
+    print(i)
+end
